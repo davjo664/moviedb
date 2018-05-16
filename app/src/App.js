742962f3901307836aec170aa7c5781e03931ff7
Wark Andreas <aw131@hdm-stuttgart.de>
2018-05-16 14:19:04 +0200

    added Login.js

--- app/src/App.js
+++ app/src/App.js
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 
-import store from './store'
-import Movies from './components/Movie';
+import 'typeface-roboto'
 
-class App extends Component {
+import store from './store'
+import Movies from './components/Movie'
+import Login from './components/Login'
+
+/*class App extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -19,6 +22,19 @@ class App extends Component {
         </div>
       </Provider>
     );
+  }
+}*/
+
+class App extends Component {
+  render() {
+    return (
+
+      <div>
+        <h1>Welcome to MovieDB</h1>
+
+        <Login/>
+      </div>
+    )
   }
 }
 
