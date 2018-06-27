d5b983692f583830357b915eefaf1f4f8a0ff33f
Wark Andreas <aw131@hdm-stuttgart.de>
2018-06-27 08:55:59 +0200

    changed wallpaper in Login

--- app/src/screens/Login.js
+++ app/src/screens/Login.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Background from '../images/Movie-Poster-Wallpaper.jpg';
+import Background from '../images/Movie-Poster-Background.jpg';
 
 class Login extends Component {
 
